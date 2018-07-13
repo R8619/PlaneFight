b98065c532eebb09e1573861b4dc3d7807afbdae
AaronGrity <1071641802@qq.com>
2018-07-13 09:25:25 +0800

    Activity exits bugs

--- app/src/main/java/com/wlw2_11/planefight/Activity/SecondActivity.java
+++ app/src/main/java/com/wlw2_11/planefight/Activity/SecondActivity.java
@@ -1,7 +1,6 @@
 package com.wlw2_11.planefight.Activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
